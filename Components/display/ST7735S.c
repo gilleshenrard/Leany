f920ff269f35aa050504ff22b89f65490b86ab89
Gilles Henrard <gilles.henrard@hotmail.com>
2024-09-08 22:38:12 +0200

    add comments

--- Components/display/ST7735S.c
+++ Components/display/ST7735S.c
@@ -1,6 +1,22 @@
+/**
+ * @file ST7735S.c
+ * @brief Implement the functioning of the ST7735S TFT screen via SPI and DMA
+ * @author Gilles Henrard
+ * @date 08/09/2024
+ *
+ * @note Datasheet : https://www.displayfuture.com/Display/datasheet/controller/ST7735.pdf
+ */
 #include "ST7735S.h"
-#include <errorstack.h>
+#include "errorstack.h"
 
+/**
+ * @brief Initialise the ST7735S display
+ *
+ * @param handle        SPI handle used
+ * @param dma           DMA handle used
+ * @param dmaChannel    DMA channel used to send data to the ST7735S
+ * @return Success
+ */
 errorCode_u st7735sInitialise(SPI_TypeDef* handle, DMA_TypeDef* dma, uint32_t dmaChannel) {
     (void)handle;
     (void)dma;
@@ -9,6 +25,11 @@ errorCode_u st7735sInitialise(SPI_TypeDef* handle, DMA_TypeDef* dma, uint32_t dm
     return (ERR_SUCCESS);
 }
 
+/**
+ * @brief Run the state machine
+ *
+ * @return Return code of the current state
+ */
 errorCode_u st7735sUpdate(void) {
     return (ERR_SUCCESS);
 }
