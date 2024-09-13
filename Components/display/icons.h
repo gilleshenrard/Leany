49068aabf4265361ff03d1a16e9edb488fed18a7
Gilles Henrard <gilles.henrard@hotmail.com>
2024-09-13 12:36:12 +0200

    start implementing Verdana 48 characters

--- Components/display/icons.h
+++ Components/display/icons.h
@@ -3,10 +3,28 @@
 #include <stdint.h>
 
 enum {
-    TEST_PATTERN_SIZE = 32U,
-    ICON_LINE_SIZE    = 32U * 2U,
+    VERDANA_NB_ROWS    = 49U,
+    VERDANA_NB_COLUMNS = 39U,
 };
 
-extern const uint32_t testPattern[TEST_PATTERN_SIZE];
+typedef enum {
+    VERDANA_0 = 0,
+    VERDANA_1,
+    VERDANA_2,
+    VERDANA_3,
+    VERDANA_4,
+    VERDANA_5,
+    VERDANA_6,
+    VERDANA_7,
+    VERDANA_8,
+    VERDANA_9,
+    VERDANA_PLUS,
+    VERDANA_MIN,
+    VERDANA_COMMA,
+    VERDANA_DEG,
+    NB_CHARACTERS,
+} verdanaCharacter_e;
+
+extern const uint64_t verdana_48ptBitmaps[NB_CHARACTERS][VERDANA_NB_ROWS];
 
 #endif
