67343f25713873b7ccea0fbeda503c0312a44b4b
Gilles Henrard <gilles.henrard@hotmail.com>
2024-09-18 16:00:34 +0200

    recreate IOC file from scratch

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -102,11 +102,11 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DMA_Init();
-  MX_SPI2_Init();
+  MX_ADC1_Init();
   MX_IWDG_Init();
   MX_SPI1_Init();
+  MX_SPI2_Init();
   MX_USART1_UART_Init();
-  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
   LL_SYSTICK_EnableIT();
   createLSM6DSOTask(SPI1);
