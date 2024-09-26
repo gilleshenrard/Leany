7a0a1949435c6fbf95adf599e7e6431226a05731
Gilles Henrard <gilles.henrard@hotmail.com>
2024-09-26 23:13:14 +0200

    Revert "implement a sendMessage() function"
    This reverts commit 8cace03600e207cba2a7ee4ffe3520c9ff6a6e5a.

--- Components/display/ST7735S.h
+++ Components/display/ST7735S.h
@@ -2,8 +2,10 @@
 #define INC_HARDWARE_SCREEN_ST7735S_H_
 
 #include <stdint.h>
+#include "FreeRTOS.h"
 #include "ST7735_initialisation.h"
 #include "errorstack.h"
+#include "queue.h"
 #include "stm32f103xb.h"
 
 enum {
@@ -23,9 +25,10 @@ typedef struct {
     int16_t     value;
 } __attribute__((aligned(MESSAGE_ALIGNMENT))) displayMessage_t;
 
+extern QueueHandle_t messageStack;
+
 errorCode_u createST7735Stask(SPI_TypeDef* handle, DMA_TypeDef* dma, uint32_t dmaChannel);
 errorCode_u st7735sSetOrientation(orientation_e orientation);
 BaseType_t  sendDisplayMessage(const displayMessage_t* message);
 void        st7735sDMAinterruptHandler(void);
-uint8_t     sendDisplayMessage(displayMessage_t message);
 #endif
